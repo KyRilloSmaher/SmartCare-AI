fddcd08fbaab63e00ceac8ba364a61df2de90400
kyrillos7 <156196824+KyRilloSmaher@users.noreply.github.com>
2026-05-06 17:18:39 +0300

    Add scikit-learn in requiremnts

--- App/ML/ai_models/LogisticRegression/intent_factory.py
+++ App/ML/ai_models/LogisticRegression/intent_factory.py
@@ -1,3 +1,5 @@
+# App/ML/ai_models/LogisticRegression/intent_factory.py
+from pathlib import Path
 from App.ML.ai_models.LogisticRegression.intent_sklearn_model import SklearnIntentModel
 from App.ML.ai_models.LogisticRegression.base import BaseIntentModel
 
@@ -5,5 +7,14 @@ def get_intent_model() -> BaseIntentModel:
     """
     Central switch for intent detection model
     """
-
-    return SklearnIntentModel("App/ML/ai_models/LogisticRegression/intent_model.pkl")
+    # Get the current file's directory
+    current_dir = Path(__file__).parent
+    
+    # Build path to model (relative to this file)
+    model_path = current_dir / "intent_model.pkl"
+    
+    #Verify model exists
+    if not model_path.exists():
+        raise FileNotFoundError(f"Model not found at: {model_path}")
+    
+    return SklearnIntentModel(str(model_path))
